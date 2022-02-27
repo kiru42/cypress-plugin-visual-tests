f08dc9e5067ef7239c6c1e2e30e693036843a84e
Kiruban Premkumar <kiru42@gmail.com>
2022-02-27 11:30:34 +0100

    chore: fix paths

--- src/paths.js
+++ src/paths.js
@@ -1,8 +1,8 @@
 module.exports = {
-  PATH_BASE64_JS: 'js-base64',
+  PATH_BASE64_JS: './node_modules/js-base64/base64.js',
   PATH_CSS: './assets/styles.css',
   PATH_DIFF_CSS: './node_modules/diff2html/bundles/css/diff2html.min.css',
   PATH_DIFF_JS: './node_modules/diff2html/bundles/js/diff2html.min.js',
   PATH_JS: './assets/script.js',
-  PATH_SOCKET_JS: 'socket.io-client/dist/socket.io.js',
+  PATH_SOCKET_JS: './node_modules/socket.io-client/dist/socket.io.js',
 };
