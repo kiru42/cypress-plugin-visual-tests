2ff73aac3012d9f57f2830224fcf631b1b4ad0ed
Kiruban Premkumar <kiru42@gmail.com>
2022-02-27 17:58:32 +0100

    fix: add assets

--- src/paths.js
+++ src/paths.js
@@ -1,8 +1,8 @@
 module.exports = {
   PATH_BASE64_JS: 'js-base64',
   PATH_CSS: './assets/styles.css',
-  PATH_DIFF_CSS: './node_modules/diff2html/bundles/css/diff2html.min.css',
-  PATH_DIFF_JS: './node_modules/diff2html/bundles/js/diff2html.min.js',
+  PATH_DIFF_CSS: './assets/diff2html/bundles/css/diff2html.min.css',
+  PATH_DIFF_JS: './assets/diff2html/bundles/js/diff2html.min.js',
   PATH_JS: './assets/script.js',
-  PATH_SOCKET_JS: 'socket.io-client/dist/socket.io.js',
+  PATH_SOCKET_JS: './assets/socket.io-client/dist/socket.io.js',
 };
