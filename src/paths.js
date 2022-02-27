ad831eadf0445b050e1d20004f570a23514d0bf0
Kiruban Premkumar <kiru42@gmail.com>
2022-02-27 13:06:13 +0100

    chore: rollback diff2html to 2.12.2

--- src/paths.js
+++ src/paths.js
@@ -1,8 +1,8 @@
 module.exports = {
   PATH_BASE64_JS: './node_modules/js-base64/base64.js',
   PATH_CSS: './assets/styles.css',
-  PATH_DIFF_CSS: './node_modules/diff2html/bundles/css/diff2html.min.css',
-  PATH_DIFF_JS: './node_modules/diff2html/bundles/js/diff2html.min.js',
+  PATH_DIFF_CSS: './node_modules/diff2html/dist/diff2html.css',
+  PATH_DIFF_JS: './node_modules/diff2html/dist/diff2html.js',
   PATH_JS: './assets/script.js',
   PATH_SOCKET_JS: './node_modules/socket.io-client/dist/socket.io.js',
 };
